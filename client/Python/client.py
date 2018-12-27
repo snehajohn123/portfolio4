c64f7362e4ce036236b6fc2ccf56316a701bf25b
“sneha” <snehaajk1995@gmail.com>
2018-12-27 17:51:49 +0000

    initial commit

---
+++ client/Python/client.py
@@ -0,0 +1,24 @@
+#Base Python client for MEng in IoT Assignment
+#consumes data from IoT Gateway
+
+import urllib.request
+import numpy as np
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+
+response = urllib.request.urlopen('http://localhost:8080/')
+resp = response.read().decode()
+
+time=[]
+temp=[]
+
+head = ET.fromstring(resp)
+for reading in head.findall('reading'):
+    time.append(reading.find('time').text)
+    temp.append(int(reading.find('temperature').text))
+
+plt.title("Temperature vs Time", loc='center')
+plt.plot(time,temp)
+plt.xlabel("time ")
+plt.ylabel("Temperature");
+plt.show()
